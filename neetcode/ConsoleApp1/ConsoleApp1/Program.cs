72529dbbd4a75919af4f4980b9039455e3ee9cdc
mhmodfrmwi <mahmoudframawi98@gmail.com>
2026-02-04 23:43:05 +0200

    Init NeetCode roadmap with Contains Duplicate solution
    - Added project files for the new repository.
    - Solved the first problem: Contains Duplicate (LeetCode 217) using a Hash Map.

---
+++ neetcode/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        #region https://leetcode.com/problems/contains-duplicate/description/
+        public bool ContainsDuplicate(int[] nums)
+         {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            
+            {
+                if (dic.ContainsKey(nums[i])) { 
+                    return true;
+                }
+                dic.Add(nums[i],1);
+            }
+            return false;
+         }
+        #endregion
+        static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+}
